ef21f59402ac93dcdf7fd1ec9978942423111cce
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2019-03-27 07:26:42 +0100

    Add comment on 'old' method

--- src/test/groovy/roomreservation/PersonHibernateSpec.groovy
+++ src/test/groovy/roomreservation/PersonHibernateSpec.groovy
@@ -8,11 +8,15 @@ class PersonHibernateSpec extends HibernateSpec {
         // Insert
         setup: 'You instantiate a person with name and an email address which has been never used before'
         def person = new Person(firstName: 'Christian', lastName: 'Ribeaud', email: 'christian.ribeaud@fhnw.ch')
+        // Assert we do NOT have any person yet
+        assert Person.count() == 0
+
         when: 'person is valid instance'
         person.validate()
         then: 'we can save it, and we get back a not null GORM Entity'
         person.save()
         and: 'there is one additional Person'
+        // 'old()': BEFORE the 'when' is executed
         Person.count() == old(Person.count()) + 1
         and: 'An UID has been automatically generated'
         person.id == 1
