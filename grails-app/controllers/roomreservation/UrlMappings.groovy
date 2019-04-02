3abc1cd2112525e77eb3a36968dd0231a5f35f2e
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2019-04-02 09:18:50 +0200

    Move URL mappings to domain models

--- grails-app/controllers/roomreservation/UrlMappings.groovy
+++ grails-app/controllers/roomreservation/UrlMappings.groovy
@@ -3,10 +3,6 @@ package roomreservation
 class UrlMappings {
 
     static mappings = {
-        "/rooms"(resources: "room")
-        "/people"(resources: "person")
-        "/bookings"(resources: "booking")
-
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
