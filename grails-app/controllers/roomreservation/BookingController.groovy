2a7e29a8838fb1b2a8fd59a3e06b43c9623bc2f5
Marcel Soltermann <souti11@hotmail.com>
2019-10-28 17:40:16 +0100

    [CHG] added methods in booking controller to display bookings

--- grails-app/controllers/roomreservation/BookingController.groovy
+++ grails-app/controllers/roomreservation/BookingController.groovy
@@ -4,4 +4,16 @@ class BookingController {
 
     static scaffold = Booking
 
+    def all() {
+        def bookings = Booking.findAll()
+        respond bookings
+    }
+
+    def forDierk(){
+        //Returns all bookings made by 'dirk.koenig@fhnw.ch'
+        def booker = Person.findByEmail('dierk.koenig@fhnw.ch')
+        def bookings = Booking.findAllByBooker(booker)
+        respond bookings
+    }
+
 }
